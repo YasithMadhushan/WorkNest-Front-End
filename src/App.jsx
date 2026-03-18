2295cd11b2a2ca605a6f8ad7e594298f666da381
YasithMadhushan <yasithmadhushan416@gmail.com>
2026-03-18 22:05:56 +0530

    added login and Sign up pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-export default function App() {
+import { Routes, Route, Outlet } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import NotFound from './pages/NotFound';
+import Contact from './pages/Contact';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-blue-600">
-      Hello Tailwind!
-    </h1>
-  )
+    <ErrorBoundary>
+      <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+
+        <Route path="/freelancer-onboarding" element={<FreelancerOnboarding />} />
+
+
+        {/* Fallback Route */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </ErrorBoundary>
+  );
 }
+
+export default App;
